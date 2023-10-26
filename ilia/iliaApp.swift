76dd928a94c3d248f7dd17e7bf60ada3c1165630
Ilia Shipitsin <ilia-shipitsin@github.com>
2023-10-26 15:41:36 +0200

    initial

---
+++ ilia/iliaApp.swift
@@ -0,0 +1,17 @@
+//
+//  iliaApp.swift
+//  ilia
+//
+//  Created by runner on 10/24/23.
+//
+
+import SwiftUI
+
+@main
+struct iliaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
